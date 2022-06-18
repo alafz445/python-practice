0f2026030546c3b8c5b32eaca7087cb33f0cdc1f
Alfredo Cruz <freddycruz445@gmaile.com>
2022-06-18 11:38:33 -0500

    first commit

---
+++ practice.py
@@ -0,0 +1,36 @@
+# this is a comment
+# int
+from itertools import starmap
+from math import fabs
+
+
+1567
+-5698
+
+# string 
+t = 3
+y = 4
+
+s1 = "string"
+s2 = "string 2"
+# concatenation
+print(s1 + s2)
+
+# bool
+True
+
+
+# float
+52.63 
+-52.89
+
+print("World", '45', end=" " )
+print("World", '45', end=" " )
+print("World", '45', end=" " )
+
+drake = "world"
+world = "Ok"
+ok_Worlds = "3500"
+world = "nope"
+drake = world # drake = "nope"
+print("\n", world, ok_Worlds,)
